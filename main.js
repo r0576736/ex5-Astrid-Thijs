5b29e72676303b1b9ca258d409ccf7c2041f5c99
astrid thijs <r0576736@student.thomasmore.be>
2017-01-04 11:18:44 +0100

    Post en Get on WhiteLists, ook aangepast in storage.js!

--- main.js
+++ main.js
@@ -48,35 +48,35 @@ app.post("/Devices", function(request, response) {
     if (existingDevice) {
         response.status(409).send({
             message: "id must be unique, it's already registered",
-            link: "../devices/" + existingDevice.id
+            link: "../Devices/" + existingDevice.id
         });
         return;
     }
 
     device.id = uuid.v4();
     dal.saveDevice(device);
-    response.status(201).location("../devices/" + device.id).send();
+    response.status(201).location("../Devices/" + device.id).send();
 });
 
 
 
 //Alarms
 
-app.get("/alarms", function(request, response) {
+app.get("/Alarms", function(request, response) {
     response.send(dal.AllAlarms());
 });
 
-app.get("/alarms/:id", function(request, response) {
-    var device = dal.findAlarm(request.params.id);
-    if (device) {
-        response.send(device);
+app.get("/Alarms/:id", function(request, response) {
+    var alarm = dal.findAlarm(request.params.id);
+    if (alarm) {
+        response.send(alarm);
     } else {
         response.status(404).send();
     }
 });
 
 
-app.post("/alarms", function(request, response) { 
+app.post("/Alarms", function(request, response) { 
     var alarm = request.body;
     var errors = validation.fieldsNotEmpty(alarm, "name_drone", "location", "type_alarm", "time_alarm", "notification", "type_notification", "important_alarm");
     if (errors) {
@@ -88,10 +88,41 @@ app.post("/alarms", function(request, response) {
 
     alarm.id = uuid.v4();
     dal.saveAlarm(alarm);
-    response.status(201).location("../alarms/" + alarm.id).send();
+    response.status(201).location("../Alarms/" + alarm.id).send();
 });
 
 
+
+//WhiteLists
+
+app.get("/WhiteLists", function(request, response) {
+    response.send(dal.AllWhiteLists());
+});
+
+app.get("/WhiteLists/:id", function(request, response) {
+    var whitelist = dal.findWhiteList(request.params.id);
+    if (whitelist) {
+        response.send(whitelist);
+    } else {
+        response.status(404).send();
+    }
+});
+
+app.post("/WhiteLists", function(request, response) {
+    var whitelist = request.body;
+
+    var errors = validation.fieldsNotEmpty(whitelist, "function", "mac_address_device", "type_device");
+    if (errors) {
+        response.status(400).send({
+            message: "Following field(s) are mandatory:" + errors.concat()
+        });
+        return;
+    }
+
+    whitelist.id = uuid.v4();
+    dal.saveWhiteList(whitelist); 
+    response.status(201).location("../WhiteLists/" + whitelist.id).send();
+});
 
 
 console.log("Hello World");
